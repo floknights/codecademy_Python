faae71fc9442225e1ab73c54eb907775e1a8e589
Flo <123563998+floknights@users.noreply.github.com>
2024-03-13 14:38:04 +0000

    Update classroom_script.py

--- classroom_script.py
+++ classroom_script.py
@@ -1,5 +1,5 @@
 from classroom_roster import student_roster
-from classroom_organizer import ClassroomOrganizer
+from classroom_organiser import ClassroomOrganiser
 import itertools
 
 #lists students by name for roll call
@@ -7,13 +7,13 @@ student_roster_iterator = iter(student_roster)
 for student in student_roster:
   print(next(student_roster_iterator))
 
-organizer = ClassroomOrganizer()
+organiser = ClassroomOrganiser()
 
 #lists possible combinations for each table of 2
-print(organizer.table_combos())
+print(organiser.table_combos())
 
 #retrieves iterables for students with the favourite subject Math or Science, combines into one iterable
-afterschool_list = itertools.chain(organizer.get_students_with_subject("Math"), organizer.get_students_with_subject("Science"))
+afterschool_list = itertools.chain(organiser.get_students_with_subject("Math"), organiser.get_students_with_subject("Science"))
 
 #retrieves an iterable for possible combinations for each table of 4
 afterschool_combos = itertools.combinations(afterschool_list, 4)
