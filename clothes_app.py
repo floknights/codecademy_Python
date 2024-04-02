938f91a888060a390e921c847128730b0d2b98c8
Flo <123563998+floknights@users.noreply.github.com>
2024-04-02 17:24:02 +0100

    Update clothes_app.py

--- clothes_app.py
+++ clothes_app.py
@@ -1,11 +1,7 @@
 from helper_functions import process_csv_supplies
 from collections import deque
 
-# The first row is skipped since it only contains labels
 csv_data = process_csv_supplies()[1:]
-
-# Here is a sample of 2 elements in csv_data:
-# [ ['nylon', '10', 'unimportant'], ['wool', '1', 'important'] ]
 
 supplies_deque = deque()
 
@@ -22,3 +18,24 @@ for _ in range(25):
 ordered_unimportant_supplies = deque()
 for _ in range(10):
   ordered_unimportant_supplies.append(supplies_deque.pop())
+
+
+clothes = [('t-shirt', 'green', 'large', 9.99),
+           ('jeans', 'blue', 'medium', 14.99),
+           ('jacket', 'black', 'x-large', 19.99),
+           ('t-shirt', 'grey', 'small', 8.99),
+           ('shoes', 'white', '12', 24.99),
+           ('t-shirt', 'grey', 'small', 8.99)]
+
+from collections import namedtuple
+
+ClothingItem = namedtuple('ClothingItem', ['type', 'color', 'size', 'price'])
+
+new_coat = ClothingItem('coat', 'black', 'small', 14.99)
+
+coat_cost = new_coat.price
+
+updated_clothes_data = []
+for item in clothes:
+  updated_clothes_data.append(ClothingItem(*item))
+#print(updated_clothes_data)
