5520c39ae614502f68c13ee97bf4b2264d2babc4
Flo <123563998+floknights@users.noreply.github.com>
2024-04-02 16:30:44 +0100

    Create fauxmagerie.py

---
+++ fauxmagerie.py
@@ -0,0 +1,13 @@
+#variable
+company_name = "La Fauxmagerie"
+#tuple
+company_location = (51.52354, -0.069374)
+#list
+company_products = ['Brixton Blue', 'Camemvert', 'Balham Blue', 'Shoreditch Smoked', 'Clapton Chive', 'Fauxrata', 'Rhondda Red', 'Fondue au Fauxmage']
+
+#dictionary
+company_data = {
+  'name': company_name,
+  'location': company_location,
+  'products': company_products
+}
