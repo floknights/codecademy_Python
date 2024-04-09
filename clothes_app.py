97c8534e7a5cf2f5cfd5d92e9334cb5505406da5
Flo <123563998+floknights@users.noreply.github.com>
2024-04-09 13:40:38 +0100

    Update clothes_app.py

--- clothes_app.py
+++ clothes_app.py
@@ -169,3 +169,18 @@ print(sorted_list)
 
 sorted_list.append(2)
 print(sorted_list)
+
+
+from collections import UserString
+
+str_name = 'python powered patterned products'
+str_word = 'patterned '
+
+class SubtractString(UserString):
+  def __sub__(self, other):
+    if other in self.data:
+      self.data = self.data.replace(other, '')
+
+subtract_string = SubtractString(str_name)
+subtract_string - str_word
+print(subtract_string)
