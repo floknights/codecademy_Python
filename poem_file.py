6f4586cb5c9ed01d8b2d4141a9cd2142b06a585a
Flo <123563998+floknights@users.noreply.github.com>
2024-04-09 16:30:01 +0100

    Update poem_file.py

--- poem_file.py
+++ poem_file.py
@@ -15,3 +15,20 @@ class PoemFiles:
 
 with PoemFiles('poem.txt', 'w') as open_poem_file:
   open_poem_file.write("Hope is the thing with feathers")
+
+
+from contextlib import contextmanager
+
+@contextmanager
+def poem_files(file, mode):
+  print('Opening File')
+  open_poem_file = open(file, mode)
+  try:
+    yield open_poem_file
+  finally:
+    print('Closing File')
+    open_poem_file.close()
+
+with poem_files('poem.txt', 'a') as opened_file:
+ print('Inside yield')
+ opened_file.write('Rose is beautiful, Just like you.')
