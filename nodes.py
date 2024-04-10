5d693333b11c68a6dd7a23487a2069753a646345
Flo <123563998+floknights@users.noreply.github.com>
2024-04-10 16:22:04 +0100

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -24,3 +24,25 @@ wackos_data = dot.get_link_node().get_value()
 
 print(dots_data)
 print(wackos_data)
+
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
