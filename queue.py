1dd29b846792e0fca8cfeb81bf4b3ecf388e5836
Flo <123563998+floknights@users.noreply.github.com>
2024-04-11 17:05:55 +0100

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,7 +4,6 @@ class Queue:
   def __init__(self, max_size=None):
     self.head = None
     self.tail = None
-   
     self.max_size = max_size
     self.size = 0
     
@@ -30,3 +29,20 @@ class Queue:
       return False
     else:
       return True
+
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
+    else:
+      print("Sorry, no more room!")
+
+q = Queue()
+q.enqueue("all the fluffy kitties")
