4fa250fb4e01ed5e083232ce586cb730d6dfda95
Flo <123563998+floknights@users.noreply.github.com>
2024-04-15 12:24:49 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -6,3 +6,13 @@ class Stack():
 
   def peek(self):
     return self.top_item.get_value()
+
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    item_to_remove = self.top_item
+    self.top_item = item_to_remove.get_next_node()
+    return item_to_remove.get_value()
