01e2d70c7fd7ee8462423fb6d661ff2c9e85238d
Flo <123563998+floknights@users.noreply.github.com>
2024-04-15 18:33:01 +0100

    Update hashmap.py

--- hashmap.py
+++ hashmap.py
@@ -14,22 +14,44 @@ class HashMap:
   def assign(self, key, value):
     array_index = self.compressor(self.hash(key))
     current_array_value = self.array[array_index]
+    
     if current_array_value == None:
       self.array[array_index] = [key, value]
-    elif key == current_array_value[0]:
+      
+    if key == current_array_value[0]:
       self.array[array_index] = value
+      
     else:
       pass
 
   def retrieve(self, key):
     array_index = self.compressor(self.hash(key))
     possible_return_value = self.array[array_index]
+    
     if possible_return_value == None:
       return None
-    elif possible_return_value[0] == key:
+      
+    if possible_return_value[0] == key:
       return possible_return_value[1]
-    else:
-      pass
+    number_collisions = 1
+    
+    while current_array_value[0] != key:
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      elif current_array_value[0] == key:
+        self.array[array_index] = [key, value]
+        return
+
+      else:
+        number_collisions += 1
+
+    return
 
 hash_map = HashMap(20)
 
