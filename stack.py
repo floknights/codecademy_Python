06bfdbddcf64bba3db0595a0a1efabd42d66110e
Flo <123563998+floknights@users.noreply.github.com>
2024-04-15 12:29:42 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,18 +1,27 @@
 from stack_node import Node
 
-class Stack():
-  def __init__(self):
+class Stack:
+  def __init__(self, limit=1000):
     self.top_item = None
-
-  def peek(self):
-    return self.top_item.get_value()
-
+    self.size = 0
+    self.limit = limit
+  
   def push(self, value):
     item = Node(value)
     item.set_next_node(self.top_item)
     self.top_item = item
 
   def pop(self):
-    item_to_remove = self.top_item
-    self.top_item = item_to_remove.get_next_node()
-    return item_to_remove.get_value()
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("The stack is empty.")
+  
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    else:
+      print("The stack is empty.")
