5afb024f33a4c5150325fd29e9dc3e9f30729708
Flo <123563998+floknights@users.noreply.github.com>
2024-04-25 16:19:45 +0100

    Update hashmap.py

--- hashmap.py
+++ hashmap.py
@@ -1,7 +1,7 @@
 class HashMap:
   def __init__(self, array_size):
     self.array_size = array_size
-    self.array = [None for i in range(self.array_size)]
+    self.array = [None for item in range(array_size)]
 
   def hash(self, key, count_collisions=0):
     key_bytes = key.encode()
@@ -14,28 +14,18 @@ class HashMap:
   def assign(self, key, value):
     array_index = self.compressor(self.hash(key))
     current_array_value = self.array[array_index]
-    
-    if current_array_value == None:
-      self.array[array_index] = [key, value]
-      
-    if key == current_array_value[0]:
-      self.array[array_index] = value
-      
-    else:
-      pass
 
-  def retrieve(self, key):
-    array_index = self.compressor(self.hash(key))
-    possible_return_value = self.array[array_index]
-    
-    if possible_return_value == None:
-      return None
-      
-    if possible_return_value[0] == key:
-      return possible_return_value[1]
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
+      return
+
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
+      return
+
     number_collisions = 1
-    
-    while current_array_value[0] != key:
+
+    while(current_array_value[0] != key):
       new_hash_code = self.hash(key, number_collisions)
       new_array_index = self.compressor(new_hash_code)
       current_array_value = self.array[new_array_index]
@@ -44,17 +34,48 @@ class HashMap:
         self.array[new_array_index] = [key, value]
         return
 
-      elif current_array_value[0] == key:
-        self.array[array_index] = [key, value]
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
         return
 
-      else:
-        number_collisions += 1
+      number_collisions += 1
 
     return
 
-hash_map = HashMap(20)
+  def retrieve(self, key):
+    array_index = self.compressor(self.hash(key))
+    possible_return_value = self.array[array_index]
 
+    if possible_return_value is None:
+      return None
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
+        return possible_return_value[1]
+
+      retrieval_collisions += 1
+
+    return
+
+
+hash_map = HashMap(17)
+
+hash_map.assign("gabbro", "igneous")
+hash_map.assign("sandstone", "sedimentary")
 hash_map.assign("gneiss", "metamorphic")
 
+print(hash_map.retrieve("gabbro"))
+print(hash_map.retrieve("sandstone"))
 print(hash_map.retrieve("gneiss"))
