f50ea162611fab92d755c995f8ac673f57808bac
Flo <123563998+floknights@users.noreply.github.com>
2024-05-15 18:04:37 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -7,7 +7,19 @@ class TreeNode:
     print("Adding " + child_node.value)
     self.children.append(child_node)
 
+  def remove_child(self, child_node):
+    print("Removing " + child_node.value + " from " + self.value)
+    new_children = []
+    for child in self.children:
+      if child != child_node:
+        new_children.append(child)
+    self.children = new_children
+
 root = TreeNode("I am Root")
 child = TreeNode("a wee sapling")
+bad_seed = TreeNode("Root Rot!")
 
 root.add_child(child)
+root.add_child(bad_seed)
+
+root.remove_child(bad_seed)
