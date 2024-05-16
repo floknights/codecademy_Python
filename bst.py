d33165f1788a48dd1930c2353fbe85f8fb389228
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 12:16:43 +0100

    Update bst.py

--- bst.py
+++ bst.py
@@ -4,20 +4,30 @@ class BinarySearchTree:
     self.depth = depth
     self.left = None
     self.right = None
-    
+
   def insert(self, value):
-    if value < self.value:
-      if not self.left:
+    if (value < self.value):
+      if (self.left is None):
         self.left = BinarySearchTree(value, self.depth + 1)
         print(f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
       else:
         self.left.insert(value)
     else:
-      if not self.right:
+      if (self.right is None):
         self.right = BinarySearchTree(value, self.depth + 1)
         print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
       else:
         self.right.insert(value)
+        
+  def get_node_by_value(self, value):
+    if value == self.value:
+      return self
+    elif self.left and value < self.value:
+      return self.left.get_node_by_value(value)
+    elif self.right and value >= self.value:
+      return self.right.get_node_by_value(value)
+    else:
+      return None
   
 root = BinarySearchTree(100)
 
@@ -25,3 +35,6 @@ root.insert(50)
 root.insert(125)
 root.insert(75)
 root.insert(25)
+
+print(root.get_node_by_value(75).value)
+print(root.get_node_by_value(55))
