537cbbc48ae70c080b11e41661be385256d79318
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 14:49:28 +0100

    Update railway_graph.py

--- railway_graph.py
+++ railway_graph.py
@@ -7,6 +7,6 @@ class Graph:
     self.graph_dict[vertex.value] = vertex
 
   def add_edge(self, from_vertex, to_vertex, weight = 0):
-    self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
     if not self.directed:
-      self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
