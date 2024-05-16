d57d52dea415b970eeab658d8f62985906bf8430
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 10:54:21 +0100

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,19 +1,20 @@
-from dfs_tree_node import TreeNode, sample_root_node, print_path, print_tree
+from TreeNode import TreeNode, sample_root_node, print_path, print_tree
 
 print_tree(sample_root_node)
 
-def dfs(root, target, path = tuple()):
+def dfs(root, target, path=()):
   path = path + (root,)
+
   if root.value == target:
-    return root
+    return path
 
   for child in root.children:
-    node_found = dfs(child, target, path)
+    path_found = dfs(child, target, path)
 
-    if node_found is not None:
-      return node_found
+    if path_found is not None:
+      return path_found
 
   return None
-        
+            
 node = dfs(sample_root_node, "F")
 print(node)
