ec299be207a19d938d9cacd4e163ab26c63d05d4
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 15:00:02 +0100

    Update railway_graph.py

--- railway_graph.py
+++ railway_graph.py
@@ -12,8 +12,15 @@ class Graph:
       self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
   def find_path(self, start_vertex, end_vertex):
-    print("Searching from {0} to {1}".format(start_vertex, end_vertex))
     start = [start_vertex]
-    while start:
+    while len(start) > 0:
       current_vertex = start.pop(0)
-      print("Current vertex: " + current_vertex)
+      print("Visiting " + current_vertex)
+
+      if current_vertex == end_vertex:
+        return True
+      else:
+        vertex = self.graph_dict[current_vertex]
+        next_vertices = vertex.get_edges()
+        start += next_vertices
+    return False
