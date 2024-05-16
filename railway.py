360bf6af6178eec43d208eae821721d91605c19b
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 15:04:20 +0100

    Update railway.py

--- railway.py
+++ railway.py
@@ -1,29 +1,26 @@
 from railway_graph import Graph
 from railway_vertex import Vertex
 
-no_path_exists = True
+railway = Graph()
 
-directed_railway = Graph(True)
+callan = Vertex('callan')
+peel = Vertex('peel')
+ulfstead = Vertex('ulfstead')
+harwick = Vertex('harwick')
 
-callan_station = Vertex('callan')
-peel_station = Vertex('peel')
-ulfstead_station = Vertex('ulfstead')
-harwick_station = Vertex('harwick')
+railway.add_vertex(callan)
+railway.add_vertex(peel)
+railway.add_vertex(harwick)
+railway.add_vertex(ulfstead)
 
-directed_railway.add_vertex(callan_station)
-directed_railway.add_vertex(peel_station)
-directed_railway.add_vertex(harwick_station)
-directed_railway.add_vertex(ulfstead_station)
+railway.add_edge(peel, harwick)
+railway.add_edge(harwick, callan)
+railway.add_edge(callan, peel)
 
-directed_railway.add_edge(harwick_station, peel_station)
-directed_railway.add_edge(peel_station, callan_station)
+peel_to_ulfstead_path_exists = railway.find_path('peel', 'ulfstead')
+harwick_to_peel_path_exists = railway.find_path('harwick', 'peel')
 
-path_exists = directed_railway.find_path('harwick', 'harwick')
-print(path_exists)
-
-print("\n\n\nFinding path from harwick to callan\n")
-new_path_exists = directed_railway.find_path('harwick', 'callan')
-print(new_path_exists)
-print("\n\nTrying to find path from harwick to ulfstead\n")
-no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
-print(no_path_exists)
+print("A path exists between peel and ulfstead:")
+print(peel_to_ulfstead_path_exists)
+print("A path exists between harwick and peel:")
+print(harwick_to_peel_path_exists)
