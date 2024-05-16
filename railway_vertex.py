e93aea1049526b840f164559d13888d83b9113c8
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 14:47:36 +0100

    Update railway_vertex.py

--- railway_vertex.py
+++ railway_vertex.py
@@ -3,18 +3,8 @@ class Vertex:
     self.value = value
     self.edges = {}
 
-  def add_edge(self, vertex):
-    print("Adding edge to {}".format(vertex))
-    self.edges[vertex] = True
+  def add_edge(self, vertex, weight = 0):
+    self.edges[vertex] = weight
 
   def get_edges(self):
     return list(self.edges.keys())
-  
-grand_central = Vertex('Grand Central Station')
-forty_second_street = Vertex('42nd Street Station')
-
-print(grand_central.get_edges())
-
-grand_central.add_edge(forty_second_street.value)
-
-print(grand_central.get_edges())
