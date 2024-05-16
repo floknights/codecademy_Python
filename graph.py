62aca1d451442cc17e51874fd9dcb635f3b33430
Flo <123563998+floknights@users.noreply.github.com>
2024-05-16 14:36:44 +0100

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,18 @@
+from graph_vertex import Vertex
+
+class Graph():
+  def __init__(self, directed = False):
+    self.directed = directed
+    self.graph_dict = {}
+
+  def add_vertex(self, vertex):
+    print("Adding {}".format(vertex.value))
+    self.graph_dict[vertex.value] = vertex
+
+grand_central = Vertex("Grand Central Station")
+
+railway = Graph()
+
+print(railway.graph_dict)
+railway.add_vertex(grand_central)
+print(railway.graph_dict)
