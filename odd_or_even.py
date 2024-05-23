b3585649607cd1bc15149d3798e97ae95a747b49
Flo <123563998+floknights@users.noreply.github.com>
2024-05-23 16:56:39 +0100

    Create odd_or_even.py

---
+++ odd_or_even.py
@@ -0,0 +1,11 @@
+def odd_or_even(n, even_function, odd_function):
+  if n % 2 == 0:
+    return even_function(n)
+  else:
+    return odd_function(n)
+
+square = lambda x: x * x
+cube = lambda x: x * x * x
+
+test = odd_or_even(5, cube, square)
+print(test)
