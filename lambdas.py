6f57304ac0184f9807e3578fc11a8c9b2f979705
Flo <123563998+floknights@users.noreply.github.com>
2024-05-23 17:12:33 +0100

    Update lambdas.py

--- lambdas.py
+++ lambdas.py
@@ -10,3 +10,11 @@ print(tuple(mapped_numbers))
 
 sum = reduce(lambda x, y: x + y, nums)
 print(sum)
+
+nums2 = (2, 12, 5, 8, 9, 3, 16, 7, 13, 19, 21, 1, 15, 4, 22, 20, 11)
+
+greater_than_10_doubled = map(lambda x: x * 2, filter(lambda y: y > 10, nums2))
+print(tuple(greater_than_10_doubled))
+
+functional_way = map(lambda x: x * 3, filter(lambda y: y % 3 == 0, nums2))
+print(tuple(functional_way))
