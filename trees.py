72ce7f4c3fb43aac5afedfd261fd7bb81fa1bbea
Flo <123563998+floknights@users.noreply.github.com>
2024-05-29 11:33:44 +0100

    Update trees.py

--- trees.py
+++ trees.py
@@ -10,5 +10,11 @@ with open('trees.csv', newline = '') as csvfile:
 
   mapper = map(lambda x: tree(int(x[0]), float(x[1]), int(x[2]), float(x[3])), reader)
 
-  trees = tuple(mapper)
-  print(trees)
+  #trees = tuple(mapper)
+  #print(trees)
+
+  t = filter(lambda q: q[2] > 75, map(lambda x: tree(int(x[0]), float(x[1]), int(x[2]), float(x[3])), reader)) # filter trees with a height over 75
+  trees = tuple(t)
+
+  widest = reduce(lambda x, y: x if x.width > y.width else y, trees) # widest tree with a height over 75
+  print(widest)
