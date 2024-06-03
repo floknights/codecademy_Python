2e645b2842e7ed8a1531ebd722ebd24b731a0541
Flo <123563998+floknights@users.noreply.github.com>
2024-06-03 22:33:45 +0100

    Update multiprocessing.py

--- multiprocessing.py
+++ multiprocessing.py
@@ -2,12 +2,24 @@ import time
 import multiprocessing
 
 def greeting_with_sleep(string):
-  s = time.perf_counter()
   print(string)
   time.sleep(2)
-  print("says hello!")
+  print(string + " says hello!")
+
+def main_multiprocessing():
+  s = time.perf_counter()
+  processes = []
+  greetings = ['Codecademy', 'Chelsea', 'Hisham', 'Ashley']
+
+  for i in range(len(greetings)):
+    p = multiprocessing.Process(target=greeting_with_sleep, args=(greetings[i],))
+    processes.append(p)
+    p.start()
+
+  for p in processes:
+    p.join()
+
   elapsed = time.perf_counter() - s
   print("Multiprocessing Elapsed Time: " + str(elapsed) + " seconds")
 
-p = multiprocessing.Process(target=greeting_with_sleep, args=('Codecademy',))
-p.start()
+main_multiprocessing()
