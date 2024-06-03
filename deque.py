db48eaa7a57fd0985a5fb8d0b196c2e5b5ea8c32
Flo <123563998+floknights@users.noreply.github.com>
2024-06-03 23:13:03 +0100

    Create deque.py

---
+++ deque.py
@@ -0,0 +1,36 @@
+class Deque:
+  def __init__(self):
+    self.elements = []
+  def add_first(self, item):
+    self.elements.append(item)
+    
+  def add_last(self, item):
+    pass
+    
+  def remove_first(self):
+    pass
+    
+  def remove_last(self):
+    pass
+    
+  def is_empty(self):
+    pass
+    
+  def size(self):
+    pass
+    
+  def peek_first(self):
+    return self.elements[-1]
+    
+  def peek_last(self):
+    return self.elements[0]
+    
+  def display_deque(self):
+    print('\t | \t'.join(str(item) for item in self.elements))
+
+deque = Deque()
+
+deque.add_first(5)
+deque.add_first(20)
+
+deque.display_deque()
